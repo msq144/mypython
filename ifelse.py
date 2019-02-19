9913778ad94991892934369347b25ba9af33270e
msq144 <sadaqhath144@gmail.com>
2019-02-19 22:02:59 +0530

    update

--- ifelse.py
+++ ifelse.py
@@ -1,12 +1,21 @@
 a=int(input("enter a:"))
 b=int(input("enter b:"))
 c=int(input("enter c:"))
-if a>b and a>c:
+"""if a>b and a>c:
     print("a is big")
 elif b>a and b>c:
     print("b is big")
 elif c>a and c>b:
     print("c is big")
 else:
-    print("all are equal")
-
+    print("all are equal")"""
+if a>b:
+    if a>c:
+        print("a is big")
+    else:
+        print("c is big")
+else:
+    if b>c:
+        print("b is big")
+    else:
+        print("c is big")
